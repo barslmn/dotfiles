8131c15c10de389e846a023b30f6a343c603385f
barslmn <barslmn@gmail.com>
2021-12-31 15:15:47 +0300

    curl -> wget

--- INSTALL.sh
+++ INSTALL.sh
@@ -15,13 +15,14 @@ git pull origin main
         && sudo chmod +x /usr/local/bin/nnn
 
 # zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -q -O- https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
 # vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+mkdir -p "$HOME"/.vim/autoload \
+	&& wget -q -O "$HOME"/.vim/autoload/plug.vim \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PluginInstall +qall
 
